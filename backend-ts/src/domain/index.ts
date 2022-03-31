13e9d2223622cb4b93f7c64f4fd5a5be74e37977
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2022-03-31 14:54:15 +0300

    Add currency exchange

--- backend-ts/src/domain/index.ts
+++ backend-ts/src/domain/index.ts
@@ -1,3 +1,3 @@
 export {processCommission} from "./domain";
 export {Repository, TransactionRecord} from "./repository";
-export {Commission, CommissionRule, Transaction} from "./types";
+export {Commission, CommissionRule, Transaction, CURRENCY} from "./types";
--- backend-ts/src/services/currency-exchange.ts
+++ backend-ts/src/services/currency-exchange.ts
@@ -1,9 +1,30 @@
+import {CURRENCY} from "../domain";
+
 /**
- *
+ * Converts amouts from a number of currencies to EURO.
  */
 export interface CurrencyExchangeService {
   convert: ({amount, sourceCurrency}: {
     amount: string
-    sourceCurrency:
-  }) => void
+    sourceCurrency: CURRENCY
+  }) => {
+    amount: string
+    sourceCurrency: CURRENCY.EURO
+  }
+}
+
+export const createCurrencyExchangeService = (): CurrencyExchangeService => {
+  return {
+    convert: ({amount, sourceCurrency}: {
+      amount: string
+      sourceCurrency: CURRENCY
+    }) => {
+
+
+      return {
+        amount: "1",
+        sourceCurrency: CURRENCY.EURO
+      }
+    }
+  }
 }

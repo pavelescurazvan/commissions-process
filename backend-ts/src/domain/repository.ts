070251b4d16a709f5ff395578f60099a18b52069
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2022-03-31 14:44:48 +0300

    Define the repository

---
+++ backend-ts/src/domain/repository.ts
@@ -0,0 +1,23 @@
+import {Transaction} from "./index";
+
+export interface TransactionRecord extends Transaction {
+  cachedAmount: number
+}
+
+export interface Repository {
+  put: (transaction: Transaction) => Promise<void>,
+  get: (clientId: number, calendarMonth: string) => Promise<TransactionRecord[]>,
+}
+
+// trans 1 -- details - user id 1 - calendarMonth - currentTotalAmount
+// trans 2 -- details - user id 1 - calendarMonth - currentTotalAmount
+// trans 3 -- details - user id 1 - calendarMonth - currentTotalAmount
+// trans 4 -- details - user id 2 - calendarMonth - currentTotalAmount
+// trans 5 -- details - user id 3 - calendarMonth - currentTotalAmount
+// trans 6 -- details - user id 4 - calendarMonth - currentTotalAmount
+// trans 7 -- details - user id 1 - calendarMonth - currentTotalAmount
+// trans 8 -- details - user id 1 - calendarMonth - currentTotalAmount
+
+// composite index on user_id + calendarMonth
+// select * from transactions where user_id === 1 AND (date < "X" AND date > "Y");
+// compute the sum based on the select

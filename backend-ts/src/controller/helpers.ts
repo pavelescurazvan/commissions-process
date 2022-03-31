910d0925f96bc83a9f850716d6ae80d2140b57dd
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2022-03-31 19:05:21 +0300

    Fix fixed precision

--- backend-ts/src/controller/helpers.ts
+++ backend-ts/src/controller/helpers.ts
@@ -1,3 +1,3 @@
 export const convertInCents = (amount: string) => parseFloat(amount) * 100;
-export const convertFromCents = (amount: number) => String(amount / 100);
+export const convertFromCents = (amount: number) => String((amount / 100).toFixed(2));
 export const convertDateToDomainFormat = (date: Date) => date.toISOString().slice(0, 10)

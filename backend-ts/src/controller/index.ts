7ff33eb74be5c1c30b506dc38a541caea48ccdb1
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2022-03-31 15:21:00 +0300

    Add most of the request handler

--- backend-ts/src/controller/index.ts
+++ backend-ts/src/controller/index.ts
@@ -1,15 +1,41 @@
+import { RequestHandler } from "express";
+import {Repository} from "../domain";
+import {CurrencyExchangeService} from "../services/currency-exchange";
+
+
 /**
  * Creates the transaction request handler
  * @param repository
  * @param currencyExchangeService
  */
-import {Repository} from "../domain";
-
-
 export const createTransactionRequestHandler = ({ repository, currencyExchangeService }: {
   repository: Repository,
   currencyExchangeService: CurrencyExchangeService
-}) => {
+}): RequestHandler  => {
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return (req, res, next) => {
+    const {date, amount, currency, clientId} = req.body;
+
+    if (!date) throw new Error("Date is undefined"); // TODO: validate that it's actual date in the correct format
+    if (!amount) throw new Error("Amount is undefined"); // TODO: validate that it's an actual float with 2 digits precision
+    if (!currency) throw new Error("Currency is undefined"); // TODO: validate that it's an actual valid currency
+    if (!clientId || isNaN(clientId)) throw new Error("Client Id is undefined or not a number");
+
+    const exchangedAmount = currencyExchangeService.convert({
+      amountInCents: convertInCents(amount),
+      sourceCurrency: currency,
+      date
+    });
+
+    // Call domain to do stuff
+
+    res.send({
+      success: true
+    });
+  }
 
 }
+
+const convertInCents = (amount: string) => parseFloat(amount) * 100
